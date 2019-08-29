3a9dc18be4bb0342900deeecf57bbcd29ceee567
Markus Persson <persson@ustwo.com>
2019-08-29 21:55:31 +0200

    Updates

---
+++ precache-manifest.51ddffaa32c89dd99fa88ad1f8b7f68b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a71d3727d67503f0bf5b231118296d0",
+    "url": "/skylords-deck/index.html"
+  },
+  {
+    "revision": "e0835b5c02152ce938e3",
+    "url": "/skylords-deck/static/css/main.6a171956.chunk.css"
+  },
+  {
+    "revision": "b87ba9004bd1f01ca0fa",
+    "url": "/skylords-deck/static/js/2.af59c403.chunk.js"
+  },
+  {
+    "revision": "e0835b5c02152ce938e3",
+    "url": "/skylords-deck/static/js/main.9bdfb252.chunk.js"
+  },
+  {
+    "revision": "1933ed29aabac44c1794",
+    "url": "/skylords-deck/static/js/runtime~main.9bd975ec.js"
+  }
+]);
